7f14a111bc2aeccbf7fa81721a2b6eaf46dd74da
Alexandre Condamine <alexandre.condamine@outlook.fr>
2023-03-21 14:58:53 +0100

    Interface responsive permettant de visualiser une liste de tâches avec des champs correspondant au nom, à l’échéance, à l’importance (Importance très haute, Importance haute, Importance Normale, Importance Basse, Importance Très Basse) et au statut d’avancement (A faire, En cours, Fait) Close #3

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,17 +7,35 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/test', name: 'main')]
+    #[Route('/', name: 'main')]
     public function index(): Response
     {
-        
-        return new Response(
-            '
-            <html>
-                <body>
-                    <h1>Liste des tâches</h1>
-                </body>
-            </html>'
-            );
+        return $this->liste();
+    }
+    
+    #[Route('/liste', name: 'liste')]
+    public function liste(): Response
+    {
+        $listeTaches = array(
+            array("nom"=>"Acheter du pain", "importance" =>"Importance basse", "etat"=>"Fait", "echeance"=>"18/03/2023", "groupe"=>"Personnel"),
+            array("nom"=>"Débarasser le garage", "importance" =>"Importance très basse", "etat"=>"A faire", "echeance"=>"18/03/2023", "groupe"=>"Personnel"),
+            array("nom"=>"Rédiger un dossier sur les enjeux de la transition numérique", "importance" =>"Importance très haute", "etat"=>"En cours", "echeance"=>"04/06/2023", "groupe"=>"Collectif des jeunes entrepreneurs toulousains innovants et ambitieux"),
+            array("nom"=>"Remplir le formulaire d'adhésion", "importance" =>"Importance normale", "etat"=>"Fait", "echeance"=>"24/03/2023", "groupe"=>"Personnel"),
+        );
+        return $this->render('liste.html.twig',[
+            'liste_taches' => $listeTaches
+        ]);
+    }
+    
+    #[Route('/calendrier', name: 'calendrier')]
+    public function calendrier(): Response
+    {
+        return $this->render('calendrier.html.twig');
+    }
+    
+    #[Route('/projet', name: 'projet')]
+    public function projet(): Response
+    {
+        return $this->render('projet.html.twig');
     }
 }
